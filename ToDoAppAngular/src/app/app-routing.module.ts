60a0c8639c9c4cc425b6d1974250a4d89a53af96
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-25 14:38:39 -0300

    Criando os roteamentos

--- ToDoAppAngular/src/app/app-routing.module.ts
+++ ToDoAppAngular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { TaskFromComponent } from './tasks/task-from/task-from.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: TaskListComponent },
+  { path: "new", component: TaskFromComponent },
+  { path: "edit/:id", component: TaskFromComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
